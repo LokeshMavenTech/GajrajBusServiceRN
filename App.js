8e03c0b9d94c573d468644e67343e8596d3999bb
Lokesh <lokesh.das@maventechlabs.com>
2024-07-26 11:01:28 +0530

    WebView

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from 'react';
-import { View, Text, SafeAreaView, Button, Alert, Linking, Platform } from 'react-native';
-import crashlytics from '@react-native-firebase/crashlytics';
+import { View, Text, SafeAreaView } from 'react-native';
 import remoteConfig from '@react-native-firebase/remote-config';
 import SplashScreen from 'react-native-splash-screen';
-import VersionCheck from 'react-native-version-check';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   useEffect(() => {
-    // Hide the splash screen when the app is ready
     SplashScreen.hide();
-
-    // Set up remote config
     remoteConfig()
       .setDefaults({
         awesome_new_feature: 'disabled',
@@ -20,51 +16,17 @@ export default function App() {
         if (fetchedRemotely) {
           const parameters = remoteConfig().getAll();
           console.log('Configs were retrieved from the backend and activated.', parameters);
-          checkAppVersion(parameters);
         } else {
           console.log('No configs were fetched from the backend, and the local configs were already activated');
         }
       });
   }, []);
 
-  const checkAppVersion = async (remoteValues) => {
-    try {
-      const currentVersion = await VersionCheck.getCurrentVersion();
-      const latestVersionString = remoteValues?.latest_version?._value;
-      const latestVersion = parseFloat(latestVersionString);
-      const updateMessage = remoteValues?.deprecation_message?._value;
-      const appStoreUrl = remoteValues?.app_store_url?._value;
-      const alertHeader = "Update Alert!";
-
-      if (latestVersion > parseFloat(currentVersion)) {
-        Alert.alert(
-          alertHeader,
-          updateMessage,
-          [
-            {
-              text: 'Update Now',
-              onPress: () => {
-                Linking.openURL(
-                  Platform.OS === 'ios'
-                    ? appStoreUrl
-                    : VersionCheck.getPlayStoreUrl({ packageName: 'com.ksrtc.awatar.new' })
-                );
-              },
-            },
-          ],
-          { cancelable: false }
-        );
-      }
-    } catch (error) {
-      console.error('Error checking app version:', error);
-    }
-  };
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>App</Text>
-        <Button title="Test Crash" onPress={() => crashlytics().crash()} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <WebView source={{ uri: 'https://www.gajrajbus.com/' }} style={{ flex: 1 }} />
       </View>
     </SafeAreaView>
   );
