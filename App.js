e3fb9666df6d022ba7a2757d72e5d312b2e702bc
Lokesh <lokesh.das@maventechlabs.com>
2024-07-29 14:28:42 +0530

    Add parameter Ios or Android

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react';
-import { View,SafeAreaView,Platform ,Text} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, SafeAreaView, Platform, Text } from 'react-native';
 import remoteConfig from '@react-native-firebase/remote-config';
 import SplashScreen from 'react-native-splash-screen';
 import { WebView } from 'react-native-webview';
 
-const getPlatform = () => Platform.OS === 'ios' ? "ios" : "android";
-const webViewUri = getPlatform() === 'ios' ? 'https://www.gajrajbus.com/' : '';
+
 
 export default function App() {
+
+  const getPlatform = () => Platform.OS === 'ios' ? "ios" : "android";
+
+  const getAppConfig = () => {
+    return `?appPage=true&platform=${getPlatform()}`;
+  }
+
+  const webViewUri =`https://www.gajrajbus.com/${getAppConfig()}`;
+  console.log(webViewUri);
+
   useEffect(() => {
     SplashScreen.hide();
+
     remoteConfig()
       .setDefaults({
         awesome_new_feature: 'disabled',
@@ -24,15 +34,13 @@ export default function App() {
         }
       });
   }, []);
- 
-
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {webViewUri ? (
         <WebView source={{ uri: webViewUri }} style={{ flex: 1 }} />
       ) : (
-        null
+       null
       )}
     </SafeAreaView>
   );
