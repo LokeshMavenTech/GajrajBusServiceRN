7c429d3dd369c638d82035dc4a4eb93aa6aabf48
Lokesh <lokesh.das@maventechlabs.com>
2024-07-29 15:19:35 +0530

    Add parameter Ios or Android

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { View, SafeAreaView, Platform, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, Platform } from 'react-native';
 import remoteConfig from '@react-native-firebase/remote-config';
 import SplashScreen from 'react-native-splash-screen';
 import { WebView } from 'react-native-webview';
 
-
-
-export default function App() {
-
+const App = () => {
   const getPlatform = () => Platform.OS === 'ios' ? "ios" : "android";
-
   const getAppConfig = () => {
     return `?appPage=true&platform=${getPlatform()}`;
   }
-
-  const webViewUri =`https://www.gajrajbus.com/${getAppConfig()}`;
+  const webViewUri = `https://www.gajrajbus.com/${getAppConfig()}`;
   console.log(webViewUri);
 
   useEffect(() => {
     SplashScreen.hide();
-
     remoteConfig()
       .setDefaults({
         awesome_new_feature: 'disabled',
@@ -35,13 +29,21 @@ export default function App() {
       });
   }, []);
 
+
+  const SCRIPT = `
+  const meta = document.createElement('meta');
+  meta.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0');
+  meta.setAttribute('name', 'viewport');
+  document.head.appendChild(meta);
+  true;
+  `;
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {webViewUri ? (
-        <WebView source={{ uri: webViewUri }} style={{ flex: 1 }} />
-      ) : (
-       null
-      )}
+      <WebView source={{ uri: webViewUri }} javaScriptEnabled onMessage={(event) => { }}
+        injectedJavaScript={SCRIPT} />
     </SafeAreaView>
   );
 }
+
+export default App;
